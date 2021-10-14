1e8bf8a2f87dc2526976a2b82d41ba5526d8829e
Thành Nguyễn Lê <18521410@gm.uit.edu.vn>
2021-10-14 23:09:34 +0700

    first commit

---
+++ Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by Thành Nguyễn Lê on 10/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
